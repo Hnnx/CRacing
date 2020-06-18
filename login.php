5755235eb62b2221488abb8346048905b7e5dfca
Nejc <nejc.stibilj01@gmail.com>
2020-06-18 22:38:51 +0200

    Raged because CSS didn't work for some reason

--- login.php
+++ login.php
@@ -1,45 +1,59 @@
 <?php
 
-$username = $_POST['username'] ?? 'Login';
+
+$username = $errorMessage = '';
 
 if(isset($_POST['submit'])){
 
-$isMember = $username == 'mem';
+  setcookie('usernameCookie', $_POST['username'], time() + 86400 );
 
-if($isMember){
-  echo "Loggedin";
+
+  if(checkName($_POST['username'])){
+    $username = $_POST['username'];
+    $errorMessage = '';
+    header("Location: index.php");
+  }
+  else{
+  $errorMessage = "Username shoud be between 3 and 13 chars";
+  }
 }
 
+function checkName($un){
+  if(strlen($un) >= 3 && strlen($un) <= 13)
+  return true;
 }
-
-
- ?>
-
+?>
 
 
 
 
 <?php include 'inc/headNav.php'; ?>
 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style/master.css" type="text/css" >
+  </head>
+  <body>
+
+  </body>
+</html>
+
+
 <div class="container">
   <div class="slim">
-    <img src="img/logo.png" class="round" width="250px"  alt="login_logo">
+    <img src="img/logo.png" class="round" alt="login_logo">
     <form class="slimForm" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
       <div class="indent">
-      <br>
-      <span class="glyphicon glyphicon-user sqr"></span>
-          <input type="text" name="username" value="">
-      <br>
-      <input type="submit" name="submit" value="LOGIN" class="btn btn-lg btn-danger">
-    </div>
+        <div class="spacing">
+         <span class="glyphicon glyphicon-user sqr"></span>
+        <input type="text" name="username" value="">
+        <p style="color:#ff030e"><?php echo $errorMessage; ?></p>
+        </div>
+        <input type="submit" name="submit" value="LOGIN" class="btn btn-lg btn-danger">
+      </div>
     </form>
   </div>
-</div>
-
-<div class="container below">
-  <p>test</p>
-
-  <p>
-
-  <?php echo htmlspecialchars($username); ?></p>
 </div>
