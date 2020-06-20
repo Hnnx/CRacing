1012a2dc4c22fdbf4f8dafd7c632057bd4cef7b4
Nejc <nejc.stibilj01@gmail.com>
2020-06-20 13:28:55 +0200

    fix rows/cols

--- inc/routes.php
+++ inc/routes.php
@@ -13,9 +13,9 @@ $tracks = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
 <div class="container">
   <div class="row">
     <?php foreach ($tracks as $track): ?>
-    <div class="col-lg-2">
-        <div class="card blackbg" style="width: 18rem;">
-          <img src="img/ant2.png" class="card-img-top rounded border border-white opaque" alt="...">
+    <div class="col-lg-4">
+        <div class="card blackbg" style="width: 25rem;">
+          <img src="img/logo.png" class="card-img-top rounded border border-white opaque" alt="...">
           <div class="card-body cardSpacing">
             <h5 class="card-title trackTitle"><?php echo htmlspecialchars($track['title']); ?> </h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
