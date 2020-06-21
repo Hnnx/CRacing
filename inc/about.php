520dbd68720065a1b1a0145b0c51816111fe7c20
Nejc <nejc.stibilj01@gmail.com>
2020-06-21 17:29:40 +0200

    +footer

--- inc/about.php
+++ inc/about.php
@@ -1,4 +1,4 @@
-<div class="container about">
+<div class="container about footerStyle">
 <p>This simple page was made to test PHP settings - Database connectivity, some light bootstrap and just to freshen up what I've learned through various tutorials.</p>
 <p>
 
@@ -14,3 +14,4 @@
 <p>Tracks/Routes should eventually be placed in the databse from every registered user - unregistred users should be able to view routes, sort them by length,
 difficulty, location, maybe some sort of description..</p>
 </div>
+<div class="spacer"></div>
--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,76 @@
-<div class="container about">
+<div class="container about footerStyle">
+
+  <p class="d-flex justify-content-center spaceTop"> BUILT WITH </p>
+  <div class="row">
+    <div class="col-sm-3">
+      <div class="d-flex flex-column mx-auto turnRed">
+        <i class="fab fa-html5 mx-auto p-3" style="font-size:4rem"></i>
+        <div class="mx-auto">
+          <p>HTML5</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-3">
+      <div class="d-flex flex-column mx-auto turnRed">
+        <i class="fab fa-css3-alt mx-auto p-3" style="font-size:4rem"></i>
+        <div class="mx-auto">
+          <p>CSS 3</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-3">
+      <div class="d-flex flex-column mx-auto turnRed">
+        <i class="fab fa-php mx-auto p-3" style="font-size:4rem"></i>
+        <div class="mx-auto">
+          <p>PHP</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-3">
+      <div class="d-flex flex-column mx-auto turnRed">
+        <i class="fa fa-database mx-auto p-3" style="font-size:4rem"></i>
+        <div class="mx-auto">
+          <p>MySQL</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <p class="d-flex justify-content-center spaceTop"> CONTACT </p>
+
+  <div class="row">
+    <div class="col-sm-6">
+      <a href="https://github.com/Hnnx">
+        <div class="d-flex flex-column mx-auto">
+          <i class="fab fa-github mx-auto p-3" style="font-size:4rem"></i>
+          <div class="mx-auto">
+            <p>HNNX@GITHUB</p>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="col-sm-6">
+      <a href="mailto:nejc.stibilj01@gmail.com?subject=(Sent from CRacing website)">
+        <div class="d-flex flex-column mx-auto">
+          <i class="fas fa-at mx-auto p-3" style="font-size:4rem"></i>
+          <div class="mx-auto">
+            <p>NEJC.STIBILJ01@GMAIL.COM</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <p class="d-flex justify-content-center spaceTop"> &copy; 2020 HNNX </p>
+
+
+
+
 
 </div>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
 </body>
 </html>
